72a53785338877a3f5ea9d1f0c3a4f3fb7b3dd1c
Tasneem-Ahmed-Mafhmoud <tasneemamed1999@gmail.com>
2023-02-06 15:21:14 +0200

    lab_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    PortfolioComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+ imgsrc:string='https://images.pexels.com/photos/1069798/pexels-photo-1069798.jpeg?auto=compress&cs=tinysrgb&w=1600';
+name:string='Katie Reed'
+skill:string='Web Develepor & Designer'
+
+ 
+
+}
